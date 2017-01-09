6203129f13dbd6007e22b9f49ec4ad21039adb71
Anders Kiel Hovgaard <akhovg@gmail.com>
2017-01-09 10:19:35 +0100

    minor change

--- benchmark/nrgrep/bench.sh
+++ benchmark/nrgrep/bench.sh
@@ -7,4 +7,8 @@ do
   /usr/bin/time -f "%e" $BIN -c -k "${i}s" "TGCAAGCGTTAAT" ../hg18/chr1.fa \
     > /dev/null
   # extract number of matches: | cut -d ' ' -f 2
-done &> nrgrep.out
+done &> nrgrep_short.out
+
+# repetitions does not appear to be supported by NR-grep so the pattern
+#   /GGGTGCAAGCGTTAAT/<k>{50,350} /AGCGTGGGGAGCAAAC/<k>
+# is highly inconvenient to run with NR-grep
