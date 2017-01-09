b89a672b18a144d66222a5e1cb6e51c5da523a0e
Ginko-X <tentenxue@gmail.com>
2017-01-09 21:08:43 +0100

    nrgrep doesn't support specifying the length of pattern??

--- benchmark/nrgrep/bench.sh
+++ benchmark/nrgrep/bench.sh
@@ -8,7 +8,7 @@ for i in {0..5}; do
     $DATA > /dev/null
   # extract number of matches: | cut -d ' ' -f 2
 done &> nrgrep_short.out
-
+.
 for i in {0..5}; do
   echo -n $i ' '
   /usr/bin/time -f "%e" $BIN -c -k "${i}s" "GCCCAGAGAACTTTCAGGATGACACCAGCAAGG" \
@@ -24,3 +24,8 @@ done &> nrgrep_alternation.out
 # repetitions does not appear to be supported by NR-grep so the pattern
 #   /GGGTGCAAGCGTTAAT/<k>{50,350} /AGCGTGGGGAGCAAAC/<k>
 # is highly inconvenient to run with NR-grep
+
+# It seems nrgrep doesnot support specifying the length range of pattern
+# A possible solution could be using the pattern (...|....) to denote /./{3,4}
+# But looks weird, may be done by some scripts 
+
