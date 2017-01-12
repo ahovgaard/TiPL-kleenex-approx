3d2c3e2912580a2d81b4f0eb9e1a8dbc291b5e56
Anders Kiel Hovgaard <akhovg@gmail.com>
2017-01-12 14:12:51 +0100

    finished scan_for_matches benchmark script except for alternation and repetitions

--- benchmark/sfm/bench.sh
+++ benchmark/sfm/bench.sh
@@ -3,19 +3,39 @@ BIN=./scan_for_matches/scan_for_matches
 DATA=../hg18/chr1.fa
 K=5
 
-#for i in {0..3}
-#do
-#  echo -n $i ' '
-#  /usr/bin/time -f "%e" $BIN pat$i.txt < ../hg18/chr1.fa > /dev/null
-#done
-
-
 declare -a progs=("short" "long" "alternation" "ranges")
 
 echo "Generating scan_for_matches pattern files..."
 for prog in "${progs[@]}"; do
   echo "  $prog"
-  for ((i=0;i<=K;i++)); do
-    ./gen.sh template_$prog.txt $i > $prog$i
+  for ((k=0;k<=K;k++)); do
+    ./gen.sh template_$prog.txt $k > $prog$k
   done
 done
+
+# incr-length
+echo "  incr-length"
+for ((i=0;i<=50;i=$i+10)); do
+  for ((k=0;k<=K;k++)); do
+    ./gen_incr-length.sh template_incr-length.txt $k $i > incr-length${k}_${i}
+  done
+done
+echo
+
+echo "Running..."
+for prog in "${progs[@]}"; do
+  echo "  $prog"
+  for ((k=0;k<=K;k++)); do
+    echo -n $k ' '
+    /usr/bin/time -f "%e" $BIN $prog$k < $DATA > /dev/null
+  done &> sfm_$prog.out
+done
+
+# incr-length case
+echo "  incr-length"
+for ((i=0;i<=50;i=$i+10)); do
+  for ((k=0;k<=K;k++)); do
+    echo -n $i $k ' '
+    /usr/bin/time -f "%e" $BIN incr-length${k}_${i} < $DATA > /dev/null
+  done
+done &> sfm_incr-length.out
