00b9251c07b81d3bf6e9e4e93afb8435660ad491
Anders Kiel Hovgaard <akhovg@gmail.com>
2017-01-16 11:58:12 +0100

    tried to implement remaining python cases

--- benchmark/python/approx_match.py
+++ benchmark/python/approx_match.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python
+# Based on code by Rasmus Fonseca from B.Sc. thesis of Maibom et. al.
 import regex
 import sys
 
-with open(sys.argv[2]) as myfile:
-    sequence = myfile.read().replace('\n',' ').upper()
-p = regex.compile(" (?:TGCAAGCGTTAAT){s<=" + sys.argv[1] + "}")
-print(len(p.findall(sequence, overlapped=True)))
+re   = ""
+prog = sys.argv[1]
+k    = sys.argv[2]
+data = sys.argv[3]
+l    = ""
+if len(sys.argv) > 4:
+    l = sys.argv[4]  # only needed for incr-length
+
+if prog == "short":
+    re = r"(?:TGCAAGCGTTAAT){s<=" + k + "}"
+elif prog == "long":
+    re = r"(?:GCCCAGAGAACTTTCAGGATGACACCAGCAAGG){s<=" + k + "}"
+elif prog == "alternation":
+    re = r"(?:GCCCAGAGA|ACTTTCAGGA){s<=" + k + "}"
+elif prog == "ranges":
+    re = r"(?:([ACGT]{5,15}TGCAAGCGTTAAT)){s<=" + k + "}"
+elif prog == "repetitions":
+    re = r"((GGGTGC){s<=" + k + "}){10,50}(AGCGT){s<=" + k + "}"
+elif prog == "incr-length":
+    re = r"((.){" + l + "}TGCAAGCGTTAAT){s<=" + k + "}"
+else:
+    print("unknown program: " + prog)
+    sys.exit(1)
+
+with open(data) as myfile:
+    sequence = myfile.read().replace('\n','').upper()
+p = regex.compile(re)
+print(p.findall(sequence, overlapped=False))
